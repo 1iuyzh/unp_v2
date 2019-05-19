72b5b5074721c5a7e9845c051b1c83dbaf9765c8
liuyzh <liuyzh@tju.edu.cn>
2019-05-19 23:32:20 +0800

    190519

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# unp_v2
+学习linux编程，第二本《Unix Network Programming, Volume 2》
---
+++ unp.h
@@ -0,0 +1,12 @@
+// IPC进程间通信inter process communication
+// 管道pipe(仅用于父子进程) 有名管道FIFO
+// system V 消息队列message queue
+// Posix消息队列
+// RPC远程过程调用Remote Procedure Call
+
+// 同步形式
+// 记录上锁record locking
+// system V 信号量semaphore 共享内存区
+// Posix信号量semaphore 共享内存区
+// 互斥锁mutex 条件变量condition variable
+// 读写锁read-write lock
